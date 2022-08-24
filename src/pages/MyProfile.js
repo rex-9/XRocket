906e62d54381915e6838f6a83962c02e308b6826
Bahoz <bahozali50@gmail.com>
2022-08-24 12:19:31 +0300

    dipslay resered rocekts in the profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -4,27 +4,33 @@ import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const allMissions = useSelector((state) => state.missions);
-  const joinedMissions = allMissions.filter((mission) => mission.joined === true);
+  const joinedMissions = allMissions.filter(
+    (mission) => mission.joined === true
+  );
   let missions = [];
   if (joinedMissions !== null) {
     missions = joinedMissions;
   }
+
+  // rockets
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved === true);
 
   return (
     <>
       <div className="profile">
         <div className="missionProfiles">
           <h1>My Missions</h1>
-          {
-            missions.map((mission) => <div key={mission.id}>{mission.name}</div>)
-          }
+          {missions.map((mission) => (
+            <div key={mission.id}>{mission.name}</div>
+          ))}
         </div>
+
         <div className="rocketProfiles">
           <h1>My Rockets</h1>
-          <div>Rocket 1</div>
-          <div>Rocket 2</div>
-          <div>Rocket 3</div>
-          <div>Rocket 4</div>
+          {reservedRockets.map((r) => (
+            <div key={r.id}>{r.name}</div>
+          ))}
         </div>
       </div>
     </>
